3aeb12e70a239289cc6ac3824f775f15fa2b575b
Sagor31h2 <sagor31h2@gmail.com>
2021-07-14 23:07:05 +0600

    Get all user

--- PaymentApp/src/app/shared/payment-detail.service.ts
+++ PaymentApp/src/app/shared/payment-detail.service.ts
@@ -10,10 +10,18 @@ export class PaymentDetailService {
   constructor(private http: HttpClient) { }
   formData: PaymentDetail = new PaymentDetail();
 
+  list: PaymentDetail[];
+
   readonly baseUrl = 'http://localhost:30130/api/PaymentDetail'
 
   postPaymentDetail() {
 
     return this.http.post(this.baseUrl, this.formData);
+  }
+
+  refreshList() {
+    this.http.get(this.baseUrl)
+      .toPromise()
+      .then(res => this.list = res as PaymentDetail[])
   }
 }
